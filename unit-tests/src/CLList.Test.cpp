ccb37feda0c1e84f60b90a2ab724d7d38355bee5
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-06 11:17:05 -0600

    Write some tests for the CLList class
    Just the beginning of a scenario class. Need to make sure that it will
    compile and run before I get too far in. The tests should all fail.

--- unit-tests/src/CLList.Test.cpp
+++ unit-tests/src/CLList.Test.cpp
@@ -1,6 +1,29 @@
 #include "../lib/Catch2/extras/catch_amalgamated.hpp"
 #include "../../include/CLList.hpp"
 
-TEST_CASE("A Simple Test", "[test]") {
-    REQUIRE(1 == 2);
+/*
+ * These are tests for the CLList Class, a class for a circularly
+ * linked list.
+ */
+
+/*
+ * This tests walks through creating a list and perform simple node operations
+ * on the list, then destroying it.
+ */
+SCENARIO("Circulalry Linked Lists can be created and destroyed", "[linked list]") {
+    GIVEN("An empty linked list") {
+        CLList<int> *list = new CLList<int>;
+
+        CHECK(list->isEmpty());
+        CHECK(list->size() == 0);
+
+        WHEN("a node is added to the list") {
+            list->add(5);
+
+            THEN("the list is no longer empty") {
+                CHECK_FALSE(list->isEmpty());
+                CHECK_FALSE(list->size() == 0);
+            }
+        }
+    }
 }
