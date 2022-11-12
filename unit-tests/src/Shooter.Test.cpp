4447ea5344b97409b3796394f4b98b2162b76d40
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-11 20:31:20 -0600

    Create Shooter.Test.cpp
    Creating a class to test the shooter class. I should be focusing on the
    lower-level classes first to make sure they work before I try to make
    sure the higher-level classes are working.

---
+++ unit-tests/src/Shooter.Test.cpp
@@ -0,0 +1,24 @@
+#include <exception>
+#define CATACH_CONFIG_MAIN
+#include "../lib/Catch2/extras/catch_amalgamated.hpp"
+#include "../../include/Shooter.hpp"
+
+/*
+ * These are tests for the CLList Class, a class for a circularly
+ * linked list.
+ */
+
+/*
+ * This tests walks through creating a list and perform simple node operations
+ * on the list, then destroying it.
+ */
+SCENARIO("Creating and deleting a shooter", "[shooter]") {
+    GIVEN("An empty shooter") {
+        Shooter *shooter = new Shooter();
+
+        THEN("the values should be defaults") {
+            REQUIRE(shooter->getName() == "");
+            REQUIRE(shooter->getQuantity() == 0);
+        }
+    }
+}
