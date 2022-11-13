ead4ead94b83f7ee9d75337c598d91641fcfe9f4
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-13 14:19:09 -0600

    Complete main.cpp
    There are a few things that I need to go back through and rewrite, but
    it should work once that is done.

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include "../include/FileManagement.hpp"
+#include "../include/CLQueue.hpp"
+
+int main(int argc, char *argv[])
+{
+    // the file that we are reading the shooters from
+    const std::string SHOOTER_FILE;
+    
+    // open the file
+    FileManagement *file = new FileManagement(SHOOTER_FILE);
+
+    // Create queue
+    CLQueue *queue = new CLQueue();
+
+    // read the file contents into a queue
+    while (!file->isEmpty()) {
+        queue->enqueue(*(new Shooter(file->getNextLine())));
+    }
+
+    // shooting sequence
+    while (!queue->isEmpty()) {
+        Shooter * temp = &queue->front();
+        queue->dequeue();
+
+        temp->shooting();
+        temp->display();
+
+        if (temp->getQuantity() != 0) {
+            queue->enqueue(*temp);
+        }
+    }
+
+    return 0;
+}
