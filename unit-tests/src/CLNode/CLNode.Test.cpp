d69408e8c78a48d7935cf529ee10d59d18dc8253
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-13 09:34:31 -0600

    Add test for nodes with non-empty shooters
    I removed the test for empty shooters since the default is to now have
    empty shooters all the time in CLNode objects. The tests passed.

--- unit-tests/src/CLNode/CLNode.Test.cpp
+++ unit-tests/src/CLNode/CLNode.Test.cpp
@@ -17,14 +17,25 @@ SCENARIO("Circularly Linked Nodes can be created and destroyed", "[node]") {
         }
     }
 
-    GIVEN("A node with an empty shooter") {
-        Shooter *shooter = new Shooter();
+    GIVEN("A node with a non-empty shooter") {
+        const std::string NAMES = GENERATE("A", "test", "shooter");
+        const int BULLETS = GENERATE(0, 1, 2, 5, 10, 21, 23);
+        Shooter *shooter = new Shooter(NAMES, BULLETS);
         CLNode *node = new CLNode();
         node->add(*shooter);
 
         THEN("the node can be deleted") {
             delete node;
             REQUIRE(true);
+        }
+
+        WHEN("copied into an empty node") {
+            CLNode *emptyNode = new CLNode(*node);
+
+            THEN("they should have matching values") {
+                REQUIRE(emptyNode->getElement() == node->getElement());
+                REQUIRE(&emptyNode->getNext() == &node->getNext());
+            }
         }
     }
 }
