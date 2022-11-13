cb1b95ed9367aea48f32753ca73e81af690be23e
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-12 20:11:47 -0600

    Create CLNode test file

---
+++ unit-tests/src/CLNode/CLNode.Test.cpp
@@ -0,0 +1,6 @@
+#define CATACH_CONFIG_MAIN
+#include "../../lib/Catch2/extras/catch_amalgamated.hpp"
+#include "../../../include/CLNode.hpp"
+
+SCENARIO("Circularly Linked Nodes can be created and destroyed") {
+}
