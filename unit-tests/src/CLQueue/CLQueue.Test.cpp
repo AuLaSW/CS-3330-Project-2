17aa99ba240f5cdc00b34af19fe70f638f60f7b7
Austin Swanlaw <austin.swanlaw@gmail.com>
2022-11-13 12:36:46 -0600

    Add first scenario to CLQueue tests
    The first scenario was written and it works, all the tests are passing.
    
    Something to keep in mind is that the queue is really just the
    circularly linked list, so much of it doesn't have to be tested. Just
    the methods that are more than calling a singular method from the CLList
    class.

--- unit-tests/src/CLQueue/CLQueue.Test.cpp
+++ unit-tests/src/CLQueue/CLQueue.Test.cpp
@@ -3,5 +3,42 @@
 #include "../../lib/Catch2/extras/catch_amalgamated.hpp"
 #include "../../../include/CLQueue.hpp"
 
-SCENARIO("", "[queue]") {
+typedef CLQueue Queue;
+SCENARIO("Creating and Deleting a queue", "[queue]") {
+    GIVEN("An empty queue") {
+        Queue *queue = new CLQueue();
+
+        THEN("the queue should be empty") {
+            REQUIRE(queue->isEmpty());
+
+            AND_THEN("the queue should have size 0") {
+                REQUIRE(queue->size() == 0);
+            }
+        }
+
+        WHEN("a new node is added to the queue") {
+            const Shooter *temp = new Shooter();
+            queue->enqueue(*temp);
+
+            THEN("that node should be the first node") {
+                REQUIRE(queue->front() == *temp);
+            }
+
+            AND_WHEN("the node is removed") {
+                queue->dequeue();
+
+                THEN("the queue should be empty") {
+                    REQUIRE(queue->isEmpty());
+                }
+            }
+        }
+
+        WHEN("the node is deleted") {
+            delete queue;
+            THEN("this assertion should pass") {
+                REQUIRE(true);
+            }
+        }
+    }
 }
+
